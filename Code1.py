63a841f404439506abd6c1c8e8b4fd704efb1a47
shehzad4 <68248511+shehzad4@users.noreply.github.com>
2021-07-25 20:52:07 -0700

    Assignment 2 Task 2

--- Code1.py
+++ Code1.py
@@ -1,1 +1,7 @@
-a=5
+Number = int(input('Enter the number:'))
+if(Number%5 == Number%3 == 0):
+    print('Consultadd-Python Training')
+elif (Number%3 == 0):
+    print('Consultadd')
+elif(Number%5 == 0):
+    print('Python Training')
